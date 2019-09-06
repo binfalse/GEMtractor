7d17dadf4f55305557856b9b81efac860264888e
Martin Scharm <martin@binfalse.de>
2019-09-06 23:37:38 +0200

    started testing with django's client

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import json
 
 # Create your tests here.
+class ApiTest(TestCase):
+  def setUp(self):
+    self.client = Client()
+  
+  def test_status_get (self):
+    response = self.client.get('/api/status')
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual("/", response["location"])
+  
+  def test_status_post (self):
+    response = self.client.post('/api/status', json.dumps({'request': 'something'}),content_type="application/json")
+    self.assertEqual(response.status_code, 200)
+    # j = json.load (response.content.decode())
+    self.assertEqual("",response.json()["answer"])
